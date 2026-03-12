9a273a2b3edca2b6ff63d22200a483a5321a788d
Fabian Herrera <contactofabianmartinez@gmail.com>
2026-03-11 22:33:18 -0600

    fix: update font classes in ContactForm component for consistency

--- src/components/contact/ContactForm.tsx
+++ src/components/contact/ContactForm.tsx
@@ -75,26 +75,26 @@ function ContactForm() {
 			<form onSubmit={handleSubmit} ref={formRef}>
 				<div className='mb-4 grid grid-cols-1 gap-4 md:grid-cols-2'>
 					<div>
-						<label htmlFor='name' className='mb-2 block font-GilroySemibold font-medium'>
+						<label htmlFor='name' className='mb-2 block font-display font-medium'>
 							{' '}
 							Nombre{' '}
 						</label>
 						<input
 							id='name'
 							placeholder='John Doe'
-							className='flex h-9 w-full rounded-md border border-input bg-transparent bg-white px-3 py-1 font-GilroyRegular text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50'
+							className='flex h-9 w-full rounded-md border border-input bg-transparent bg-white px-3 py-1 font-accent text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50'
 							required
 							name='name'
 						/>
 					</div>
 					<div>
-						<label htmlFor='email' className='mb-2 block font-GilroySemibold font-medium'>
+						<label htmlFor='email' className='mb-2 block font-display font-medium'>
 							{' '}
 							Correo{' '}
 						</label>
 						<input
 							type='email'
-							className='flex h-9 w-full rounded-md border border-input bg-transparent bg-white px-3 py-1 font-GilroyRegular text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50'
+							className='flex h-9 w-full rounded-md border border-input bg-transparent bg-white px-3 py-1 font-accent text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50'
 							placeholder='john.doe@email.com'
 							required
 							name='email'
@@ -102,20 +102,20 @@ function ContactForm() {
 					</div>
 				</div>
 				<div className='mb-4'>
-					<label htmlFor='subject' className='mb-2 block font-GilroySemibold font-medium'>
+					<label htmlFor='subject' className='mb-2 block font-display font-medium'>
 						{' '}
 						Asunto{' '}
 					</label>
 					<input
 						id='subject'
 						placeholder={null}
-						className='flex h-9 w-full rounded-md border border-input bg-transparent bg-white px-3 py-1 font-GilroyRegular text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50'
+						className='flex h-9 w-full rounded-md border border-input bg-transparent bg-white px-3 py-1 font-accent text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50'
 						required
 						name='subject'
 					/>
 				</div>
 				<div className='mb-4'>
-					<label htmlFor='message' className='mb-2 block font-GilroySemibold font-medium'>
+					<label htmlFor='message' className='mb-2 block font-display font-medium'>
 						{' '}
 						Mensaje{' '}
 					</label>
@@ -131,13 +131,13 @@ function ContactForm() {
 				<div className='flex flex-col justify-between gap-4 sm:flex-row'>
 					<input
 						type='submit'
-						className='w-full cursor-pointer bg-black py-2 font-GilroySemibold text-white hover:bg-black/80 sm:w-60'
+						className='w-full cursor-pointer bg-black py-2 font-display text-white hover:bg-black/80 sm:w-60'
 						id='submit'
 						value='Enviar'
 					/>
 					<input
 						type='reset'
-						className='text-gray-700 w-full cursor-pointer border border-neutral-200 border-neutral-300 bg-white py-2 font-GilroySemibold hover:bg-neutral-100 sm:w-60'
+						className='text-gray-700 w-full cursor-pointer border border-neutral-200 border-neutral-300 bg-white py-2 font-display hover:bg-neutral-100 sm:w-60'
 						value='Borrar'
 					/>
 				</div>
