4a5de6db68a3e9292d42aac0075dd258eb01ae57
Fabian Herrera <contactofabianmartinez@gmail.com>
2026-03-11 22:32:33 -0600

    fix: update font classes in Weather component for consistency

--- src/components/bento/Weather.tsx
+++ src/components/bento/Weather.tsx
@@ -32,33 +32,33 @@ const CurrentWeather: React.FC = () => {
 
 	return (
 		<div className='mb-4 md:mb-0'>
-			<h2 className='mt-4 pl-4 font-GilroyBold text-2xl text-white/90 md:pl-0'>{location}</h2>
+			<h2 className='mt-4 pl-4 font-heading text-2xl text-white/90 md:pl-0'>{location}</h2>
 			<div className='flex flex-col items-center justify-center pb-4'>
 				<img
 					src={`/weather/${icon}`}
 					alt={weather.weather[0].main}
 					className='size-32 select-none'
 				/>
-				<p className='font-GilroySemibold text-lg text-white'>{weather.weather[0].main}</p>
+				<p className='font-display text-lg text-white'>{weather.weather[0].main}</p>
 			</div>
 			<div className='flex flex-row items-center justify-between px-4 md:px-0'>
-				<p className='flex items-center gap-2 font-GilroySemibold text-sm text-white md:text-lg'>
+				<p className='flex items-center gap-2 font-display text-sm text-white md:text-lg'>
 					<Eye className='size-4 md:size-5' />
 					{visibility} KM
 				</p>
 				<div className='flex flex-row items-center gap-x-2 md:gap-x-4'>
-					<p className='flex items-center gap-1 font-GilroySemibold text-sm text-white md:text-lg'>
+					<p className='flex items-center gap-1 font-display text-sm text-white md:text-lg'>
 						<img src='/weather/wind.svg' alt='Wind' className='size-8 select-none md:size-12' />
 						{windSpeed} km/h
 					</p>
-					<p className='flex items-center font-GilroySemibold text-sm text-white md:text-lg'>
+					<p className='flex items-center font-display text-sm text-white md:text-lg'>
 						<img src='/weather/humidity.svg' alt='Wind' className='size-8 select-none md:size-12' />
 						{weather.main.humidity}%
 					</p>
 				</div>
 			</div>
 			<div className='flex flex-row items-center justify-between px-4 md:px-0'>
-				<p className='flex items-center font-GilroySemibold text-sm text-white md:text-lg'>
+				<p className='flex items-center font-display text-sm text-white md:text-lg'>
 					<img
 						src='/weather/thermometer-glass.svg'
 						alt='Thermometer'
@@ -67,7 +67,7 @@ const CurrentWeather: React.FC = () => {
 					{temperature}°C
 				</p>
 				<div className='flex flex-row items-center justify-end gap-x-2'>
-					<p className='flex items-center font-GilroySemibold text-sm text-white md:text-lg'>
+					<p className='flex items-center font-display text-sm text-white md:text-lg'>
 						<img
 							src='/weather/thermometer-colder.svg'
 							alt='Thermometer Colder'
@@ -75,7 +75,7 @@ const CurrentWeather: React.FC = () => {
 						/>
 						{weather.main.temp_min.toFixed(0)}°C
 					</p>
-					<p className='flex items-center font-GilroySemibold text-sm text-white md:text-lg'>
+					<p className='flex items-center font-display text-sm text-white md:text-lg'>
 						<img
 							src='/weather/thermometer-warmer.svg'
 							alt='Thermometer Warmer'
