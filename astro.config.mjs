891773cdc6c4a4128b49ad70d82bf6c809110b20
Fabian Herrera <contactofabianmartinez@gmail.com>
2026-03-11 22:31:31 -0600

    fix: update Tailwind CSS integration in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,19 @@
-import tailwind from '@astrojs/tailwind'
 import { defineConfig, envField } from 'astro/config'
 import react from '@astrojs/react'
 import vercel from '@astrojs/vercel'
 
 import partytown from '@astrojs/partytown'
 
+import tailwindcss from '@tailwindcss/vite'
+
 // https://astro.build/config
 export default defineConfig({
 	output: 'server',
+
 	adapter: vercel({
 		webAnalytics: { enabled: true },
 	}),
+
 	env: {
 		schema: {
 			IPINFO_API_KEY: envField.string({ context: 'server', access: 'secret' }),
@@ -21,15 +24,10 @@ export default defineConfig({
 		},
 		validateSecrets: true,
 	},
+
 	serverIslands: true,
 
-	integrations: [
-		tailwind({
-			applyBaseStyles: false,
-		}),
-		react(),
-		partytown(),
-	],
+	integrations: [react(), partytown()],
 
 	i18n: {
 		defaultLocale: 'es',
@@ -37,5 +35,9 @@ export default defineConfig({
 		routing: {
 			prefixDefaultLocale: false,
 		},
+	},
+
+	vite: {
+		plugins: [tailwindcss()],
 	},
 })
